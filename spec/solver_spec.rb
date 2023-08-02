dc19ea709417c38fad22b4fd92d32778500de44a
Luis Zubia <soyzubia@gmail.com>
2023-08-02 15:08:32 -0700

    we can instantiate a Solver object

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,10 @@
+# frozen_string_literal: true
+
 require_relative '../solver'
 
 describe Solver do
-  
+  it 'let us instantiate a Solver type object' do
+    s = Solver.new
+    expect(s).to be_an_instance_of Solver
+  end
 end
