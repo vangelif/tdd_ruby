6123c3bca2707447bb9713adeee1c87c15f3fb16
Luis Zubia <soyzubia@gmail.com>
2023-08-02 15:18:37 -0700

    Add test to check if 'factorial' method exists?

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,10 +1,13 @@
-# frozen_string_literal: true
-
 require_relative '../solver'
 
 describe Solver do
   it 'let us instantiate a Solver type object' do
     s = Solver.new
     expect(s).to be_an_instance_of Solver
+  end
+
+  it 'has a \'factorial\' method' do
+    s = Solver.new
+    expect(s.methods.include?(:factorial)).to be_truthy
   end
 end
