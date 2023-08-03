1b1fe79a72eed7d459db17941f21e8376b016e0c
Luis Zubia <soyzubia@gmail.com>
2023-08-02 17:15:18 -0700

    Add test to check if 'reverse' method exists?

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -3,17 +3,19 @@
 require_relative '../solver'
 
 describe Solver do
-  it 'let us instantiate a Solver type object' do
-    s = Solver.new
-    expect(s).to be_an_instance_of Solver
+  describe 'can create objects from Solver?' do
+    it 'let us instantiate a Solver type object' do
+      s = Solver.new
+      expect(s).to be_an_instance_of Solver
+    end
   end
 
-  it 'has a \'factorial\' method' do
-    s = Solver.new
-    expect(s.methods.include?(:factorial)).to be_truthy
-  end
+  describe '#factorial method' do
+    it 'has a \'factorial\' method' do
+      s = Solver.new
+      expect(s.methods.include?(:factorial)).to be_truthy
+    end
 
-  describe '#factorial' do
     it 'returns the correct factorial for positive integers' do
       s = Solver.new
       expect(s.factorial(1)).to eq(1)
@@ -21,17 +23,25 @@ describe Solver do
       expect(s.factorial(7)).to eq(5040)
       expect(s.factorial(10)).to eq(3_628_800)
     end
+
     it 'returns 1 for input 0' do
       s = Solver.new
       expect(s.factorial(0)).to eq(1)
     end
+
     it 'raises an ArgumentError' do
       s = Solver.new
       expect { s.factorial(-1) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
       expect { s.factorial(-10) }.to raise_error(ArgumentError, 'Input must be a non-negative integer')
     end
   end
-  describe '#reverse' do
+
+  describe '#reverse method' do
+    it 'has a \'reverse\' method' do
+      s = Solver.new
+      expect(s.methods.include?(:reverse)).to be_truthy
+    end
+
     it 'returns the reverse of a word' do
       s = Solver.new
 
