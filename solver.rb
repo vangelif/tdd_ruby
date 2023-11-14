7b88ebd4c6941c515e727c9b1585c2c9e226870a
Vangelis <ev.fotiades@yahoo.com>
2023-11-14 14:03:11 +0800

    Update solver.rb

--- solver.rb
+++ solver.rb
@@ -3,6 +3,9 @@
 #  > reverse - which returns a reversed version of the provided string
 #  > fizzbuzz - which returns a string according to a number provided
 class Solver
+
+  # FACTORIAL is a function that
+  # multiplies all whole numbers that preceded our given number down to 1  
   def factorial(number)
     if number.negative?
       raise ArgumentError, 'Input must be a non-negative integer'
@@ -17,6 +20,8 @@ class Solver
     word.reverse
   end
 
+  # FIZZBUZZ function is  checking whether our number is 
+  # exactly divisible by 5 or 3
   def fizzbuzz(num)
     return 'fizzbuzz' if (num % 5).zero? && (num % 3).zero?
 
